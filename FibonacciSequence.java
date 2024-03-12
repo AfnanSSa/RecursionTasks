964534be148cedf664b66128d2dc2bcb4267ecd3
AfnanSSa <iafnanz67@gmail.com>
2024-03-12 10:00:59 +0400

    Calculating the nth Fibonacci number

---
+++ FibonacciSequence.java
@@ -0,0 +1,42 @@
+/* Fibonacci Sequence
+ * Objective:
+ * Develop a Java program capable of calculating the nth Fibonacci number. The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1.
+ * Requirements:
+ * The program should use recursion to calculate the nth Fibonacci number.
+ * The first two numbers in the Fibonacci sequence are 0 and 1, respectively.
+ * The input will be an integer n, representing the position in the Fibonacci sequence.
+ * The output should be the nth Fibonacci number.
+ * Assume that the input is always a valid non-negative integer.
+ * Implement error handling for unexpected situations, such as negative inputs or inputs that are not integers.*/
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class RecursionTasks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        /*Error Handling*/
+        try{
+            System.out.println("\nEnter a number: "); //prompting user to enter a number
+            Integer input = scanner.nextInt(); //getting input
+            if (input < 0){
+                System.out.println("\nEnter a positive number."); //if input is a number less than 1
+            } else {
+                //if input is 1 or more
+                System.out.println(input + "th number in Fibonacci Sequence is: " + fibonacciSequence(input));
+            }
+        } catch (InputMismatchException exception){
+            System.out.println("\nEnter an integer."); //if input is not an integer
+        }
+    }
+
+    //method to get nth value of Fibonacci Sequence
+    public static Integer fibonacciSequence(Integer n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacciSequence(n -1) + fibonacciSequence(n-2);
+        }
+    }
+}
